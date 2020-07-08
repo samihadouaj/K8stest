de3b36ee5b69b98a37c35943dc90204be8beda33
Sami <samihadouaj@gmail.com>
2020-07-08 11:51:46 +0100

    version with PV

---
+++ runall.sh
@@ -0,0 +1,6 @@
+kubectl apply -f secret_config.yaml
+kubectl apply -f pv.yaml
+kubectl apply -f pv_claim.yaml
+kubectl apply -f mongo_config.yaml
+kubectl apply -f configMap.yaml
+kubectl apply -f mongoexpress_config.yaml
