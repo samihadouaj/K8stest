4756b93cd705e75b0756f3d84948af009126a737
Sami <samihadouaj@gmail.com>
2020-07-10 12:27:27 +0100

    yemchi

--- runall.sh
+++ runall.sh
@@ -1,6 +1,7 @@
 kubectl apply -f secret_config.yaml
-kubectl apply -f pv.yaml
-kubectl apply -f pv_claim.yaml
+# kubectl apply -f pv.yaml
+# kubectl apply -f pv_claim.yaml
+kubectl apply -f storageClass.yaml
 kubectl apply -f mongo_config.yaml
 kubectl apply -f configMap.yaml
-kubectl apply -f mongoexpress_config.yaml
+# kubectl apply -f mongoexpress_config.yaml
